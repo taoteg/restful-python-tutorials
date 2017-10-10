82083d8706eb193a0576c2823ee3bcf5b7070f18
taoteg <taoteg@gmail.com>
2017-10-10 14:21:03 -0500

    Replaced normal Serializer with ModelSerializer to simplify class code.

--- djmailer/api/serializers.py
+++ djmailer/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 
+from .models import Subscriber
 
+# Serializer Example.
+"""
 class SubscriberSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=50)
     age = serializers.IntegerField()
     email = serializers.EmailField()
+"""
+
+# ModelSerializer Example.
+class SubscriberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscriber
+        fields = "__all__"      # use all fields in the model.
