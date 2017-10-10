4d5f2bc28427ad26e03f3fa28be133191c5e670c
taoteg <taoteg@gmail.com>
2017-10-10 13:05:59 -0500

    Removed extraneous code to focus on DRF implementation class. Now handles GET and POST with name param.

--- djmailer/api/views.py
+++ djmailer/api/views.py
@@ -1,14 +1,14 @@
 # from django.shortcuts import render
-from django.http.response import JsonResponse
+# from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 
 # Create your views here.
 
 # Simple view.
-def hello_world_simple(request):
-    return JsonResponse({"msg":"hello django"})
+# def hello_world_simple(request):
+#     return JsonResponse({"msg":"hello django"})
 
 
 # APIView Class example.
@@ -16,15 +16,21 @@ class HelloWorldView(APIView):
     def get(self, request):
         return Response({"msg":"Hello Django from APIView"})
 
-
-# APIView function example.
-@api_view(["GET","POST"])
-def hello_world_dec(request):
-    if request.method == "GET":
-        return Response({"msg":"Hello django from the get method"})
-
-    else:
+    def post(self, request):
         name = request.data.get("name")
         if not name:
             return Response({"error":"No name passed"})
         return Response({"msg":"Hello {}!".format(name)})
+
+
+# APIView function example.
+# @api_view(["GET","POST"])
+# def hello_world_dec(request):
+#     if request.method == "GET":
+#         return Response({"msg":"Hello django from the get method"})
+#
+#     else:
+#         name = request.data.get("name")
+#         if not name:
+#             return Response({"error":"No name passed"})
+#         return Response({"msg":"Hello {}!".format(name)})
