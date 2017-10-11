e75e921ea5995a36f3bdd42cf2e62d83d13edec2
taoteg <taoteg@gmail.com>
2017-10-11 12:11:11 -0500

    Added jet tokens to django app. Implemented endpoint to obtain token using username and password.

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+
 from rest_framework.routers import SimpleRouter
+from rest_framework_jwt.views import obtain_jwt_token
 
 # from .views import SubscriberView
 from .views import SubscriberViewSet
@@ -18,4 +20,7 @@ router = SimpleRouter()
 router.register("subscribers", SubscriberViewSet)
 
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+urlpatterns = router.urls + [
+    url(r'^jwt-auth/', obtain_jwt_token),
+]
